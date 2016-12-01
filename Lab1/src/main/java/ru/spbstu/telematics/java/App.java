5f3796414bc6c4b76de2f31b3f72553275718626
Hamlet <hamlet96@mail.ru>
2016-12-01 05:23:05 +0300

    Lab1

---
+++ Lab1/src/main/java/ru/spbstu/telematics/java/App.java
@@ -0,0 +1,32 @@
+package ru.spbstu.telematics.java;
+
+/**
+ * Hello world!
+ *
+ */
+class Vector {
+	  public final int x;
+	  public final int y;
+	  public Vector( int x, int y){
+	    this.x = x;
+	    this.y = y;
+	  }
+	}
+
+
+public class App 
+{
+	public static int VecSum(Vector a, Vector b)
+	{
+		return a.x*b.x+a.y*b.y;
+	}
+	
+    public static void main( String[] args )
+    {
+    	
+        
+        Vector a = new Vector(5,6);
+        Vector b = new Vector(4,10);
+        System.out.println(VecSum(a,b));
+    }
+}
